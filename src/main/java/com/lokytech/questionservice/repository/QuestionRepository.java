5a08fee7117771491c78d44ded5264eb22658492
lokytech5 <lokytech5@gmail.com>
2023-10-15 02:54:25 +0100

    Added QuestionRepository class for jpa communication with database

---
+++ src/main/java/com/lokytech/questionservice/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.lokytech.questionservice.repository;
+
+import com.lokytech.questionservice.entity.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Questions, Long> {
+}
