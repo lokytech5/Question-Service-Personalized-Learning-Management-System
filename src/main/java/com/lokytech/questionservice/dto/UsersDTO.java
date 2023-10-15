65e12dd9a0707f9eef0198298df845f478bb6c0c
lokytech5 <lokytech5@gmail.com>
2023-10-15 02:51:41 +0100

    Added usersClient interface and userDTO to represent user entity in user service

---
+++ src/main/java/com/lokytech/questionservice/dto/UsersDTO.java
@@ -0,0 +1,31 @@
+package com.lokytech.questionservice.dto;
+
+public class UsersDTO {
+    private Long Id;
+    private String username;
+    private String email;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
