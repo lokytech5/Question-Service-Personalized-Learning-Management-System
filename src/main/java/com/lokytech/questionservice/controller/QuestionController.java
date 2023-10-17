f458faaa118c0f413b6f39556a92d2a3d71f8de7
lokytech5 <lokytech5@gmail.com>
2023-10-17 05:08:22 +0100

    endpoint to getUsersQuestionById was added

--- src/main/java/com/lokytech/questionservice/controller/QuestionController.java
+++ src/main/java/com/lokytech/questionservice/controller/QuestionController.java
@@ -1,12 +1,15 @@
 package com.lokytech.questionservice.controller;
 
+import com.lokytech.questionservice.dto.QuestionsDTO;
 import com.lokytech.questionservice.entity.Questions;
+import com.lokytech.questionservice.exception.UserNotFoundException;
 import com.lokytech.questionservice.service.QuestionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,5 +24,15 @@ public class QuestionController {
     public ResponseEntity<Questions> createQuestions(@PathVariable Long userId, @Valid @RequestBody Questions questions){
         Questions savedQuestion = questionService.saveQuestion(questions, userId);
         return new ResponseEntity<>(savedQuestion, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/question/{userId}")
+    public ResponseEntity<?> findQuestionByUserId(@PathVariable Long userId){
+        try{
+            QuestionsDTO question = questionService.findQuestionById(userId);
+            return new ResponseEntity<>(question, HttpStatus.OK);
+        } catch (UserNotFoundException e){
+            return  new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/lokytech/questionservice/service/QuestionService.java
+++ src/main/java/com/lokytech/questionservice/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.lokytech.questionservice.client.UsersClient;
 import com.lokytech.questionservice.dto.QuestionsDTO;
 import com.lokytech.questionservice.dto.UsersDTO;
 import com.lokytech.questionservice.entity.Questions;
+import com.lokytech.questionservice.exception.UserNotFoundException;
 import com.lokytech.questionservice.repository.QuestionRepository;
 import com.lokytech.questionservice.validator.UserExistenceValidator;
 import org.modelmapper.ModelMapper;
@@ -31,7 +32,10 @@ public class QuestionService {
         return questionRepository.save(questions);
     }
 
-    public List<Questions> findALlQuestions(Questions questions){
-        return questionRepository.findAll();
+    public QuestionsDTO findQuestionById(Long userId){
+        UsersDTO users = usersClient.getUserById(userId);
+        Questions questions = questionRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User Not Found with id" + userId));
+        return mapper.map(questions, QuestionsDTO.class);
     }
 }
