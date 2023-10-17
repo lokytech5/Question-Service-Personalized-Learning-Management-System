6e26b119c766c164e823620c4a06c376c25efd8e
lokytech5 <lokytech5@gmail.com>
2023-10-17 05:13:41 +0100

    Added require fields for AnswersService class and users constructor instead of Autowired for better encapsulation

--- src/main/java/com/lokytech/questionservice/service/AnswersService.java
+++ src/main/java/com/lokytech/questionservice/service/AnswersService.java
@@ -1,5 +1,20 @@
 package com.lokytech.questionservice.service;
 
+import com.lokytech.questionservice.repository.AnswersRepository;
+import com.lokytech.questionservice.repository.QuestionRepository;
+import com.theokanning.openai.service.OpenAiService;
+
 public class AnswersService {
+
+    private final QuestionRepository questionRepository;
+    private final AnswersRepository answersRepository;
+    private final OpenAiService openAiService;
+
+    public AnswersService(QuestionRepository questionRepository, AnswersRepository answersRepository, OpenAiService openAiService) {
+        this.questionRepository = questionRepository;
+        this.answersRepository = answersRepository;
+        this.openAiService = openAiService;
+    }
+
 
 }
