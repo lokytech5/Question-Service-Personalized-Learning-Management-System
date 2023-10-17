8e39ce711dd4057ed667d2102f219dcc3f438c9c
lokytech5 <lokytech5@gmail.com>
2023-10-17 09:01:18 +0100

    created request and respond dto class to match open-ai respond from the documentation

---
+++ src/main/java/com/lokytech/questionservice/dto/CompletionResponseDTO.java
@@ -0,0 +1,93 @@
+package com.lokytech.questionservice.dto;
+
+import java.util.List;
+
+public class CompletionResponseDTO {
+    private String id;
+    private String object;
+    private long created;
+    private String model;
+    private List<ChoiceResponseDTO> choices;
+    private UsageResponseDTO usage;
+
+    public CompletionResponseDTO() {
+    }
+
+    public CompletionResponseDTO(String id, String object, long created, String model, List<ChoiceResponseDTO> choices, UsageResponseDTO usage) {
+        this.id = id;
+        this.object = object;
+        this.created = created;
+        this.model = model;
+        this.choices = choices;
+        this.usage = usage;
+    }
+
+    public static class ChoiceResponseDTO {
+        private String text;
+        private int index;
+        private Object logprobs;
+        private String finish_reason;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public Object getLogprobs() {
+            return logprobs;
+        }
+
+        public void setLogprobs(Object logprobs) {
+            this.logprobs = logprobs;
+        }
+
+        public String getFinish_reason() {
+            return finish_reason;
+        }
+
+        public void setFinish_reason(String finish_reason) {
+            this.finish_reason = finish_reason;
+        }
+    }
+
+    public static class UsageResponseDTO {
+        private int prompt_tokens;
+        private int completion_tokens;
+        private int total_tokens;
+
+        public int getPrompt_tokens() {
+            return prompt_tokens;
+        }
+
+        public void setPrompt_tokens(int prompt_tokens) {
+            this.prompt_tokens = prompt_tokens;
+        }
+
+        public int getCompletion_tokens() {
+            return completion_tokens;
+        }
+
+        public void setCompletion_tokens(int completion_tokens) {
+            this.completion_tokens = completion_tokens;
+        }
+
+        public int getTotal_tokens() {
+            return total_tokens;
+        }
+
+        public void setTotal_tokens(int total_tokens) {
+            this.total_tokens = total_tokens;
+        }
+    }
+}
